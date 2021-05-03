91d27c7ae57d61b96c025b0d1bb794282482edcd
PermissionError <dev@rayma.me>
2021-05-03 17:49:41 +1200

    bruh

--- pages/api/graph.js
+++ pages/api/graph.js
@@ -1,19 +1,22 @@
 export default function handler(req, res) {
-  let json = JSON.parse(req.body);
-  let F = json.F;
-  let a = json.a;
-  let accelerations = [['x', 'Acceleration']];
-  let speeds = [['x', 'Speed']];
+  let mb = 0.001;
+  let p = 1.225
+  let A = 0.005344;
+  let cl = req.body.Cl;
+  let a = 0;
   let vv = 0;
-  for (let i = 0; i < 1_000_000; i++) {
-    vv = vv + a / 1000;
-    //Add current a and vv to results
-    if(i % 1000 === 0) {
-      accelerations.push([i/1000, a]);
-      speeds.push([i/1000, vv]);
-    }
-    a = (F - json.Cd * json.p * (vv ** 2) * json.A / 2) / json.m2;
+  let s = 0;
+  //TODO This is not right
+  let vh = 9.51;
+  let collection = [vh];
+  for(let i = 1; i < 10000; i++) {
+    vh = vh - (1/1000) * 0.09426816 * vh**2;
+
+    a = a + (1/1000) * ( (cl * p * vh**2 * A / (2 - 9.81 * mb)) / mb);
+    vv = vv + (1/1000) * a;
+    s = s + (1/1000) * vv;
+    collection[i] = vh;
   }
-  res.json({accelerations: accelerations, speeds: speeds});
-  res.end(200);
+
+  res.end('bruh');
 }
--- pages/mhc.js
+++ pages/mhc.js
@@ -5,6 +5,12 @@ import Chart from "react-google-charts";
 import {useRouter} from "next/router";
 
 export default function MHC() {
+  /*
+  a) Change v to vh
+b) Write vh decreasing over time
+c) In the for loop, have a reset every time, since vh will be changing this will mean that a, the rate of change vv, will change
+d) Change the scale on time
+   */
   const [accChart, setAccChart] = useState();
   const [speedChart, setSpeedChart] = useState();
 
@@ -20,19 +26,11 @@ export default function MHC() {
   }
   const calculate = (event) => {
     event.preventDefault();
-    if (event.target.r.value <= 0) {
-      event.target.r.className = event.target.r.className + ' is-invalid';
-      return;
-    }
     document.getElementById('calculating').hidden = false;
-    const G = 6.674 * (10 ** -11);
-    // Fnet = Cl × ρ x v^2 x A / 2 - G (m1×m2) / r^2
-    let F = event.target.cl.value * event.target.p.value * (event.target.v.value ** 2) * event.target.A.value / 2 - G * event.target.m1.value * event.target.m2.value / (event.target.r.value ** 2);
-    let a = F / event.target.m2.value;
 
-    fetch('/api/graph', {method: 'POST', body: JSON.stringify({F: F, a: a, Cd: event.target.Cd.value, p: event.target.p.value, A: event.target.A.value, m2: event.target.m2.value})}).then(r => {
+    fetch('/api/graph', {method: 'POST', body: JSON.stringify({Cl: event.target.Cl.value}).then(r => {
       r.json().then(data => {
-        setAccChart(
+        /*setAccChart(
           <Chart
             width={'100%'}
             height={'50vh'}
@@ -79,10 +77,11 @@ export default function MHC() {
         document.getElementById('result').hidden = false;
         document.getElementById('calculating').hidden = true;
         //TODO
-        Cookies.set('maxHeight', null);
+        Cookies.set('maxHeight', null);*/
+        data.
       });
     })
-  }
+  });
   return (
     <>
       <Container fluid={true} className={'d-flex justify-content-center align-items-center mt-2'}>
@@ -101,47 +100,9 @@ export default function MHC() {
           </div>
           <hr/>
           <h3>Parameters</h3>
-          <FormGroup className={'px-0'}>
-            <label htmlFor={'G'}>Gravitational Constant (G)</label>
-            <p id={'G'}><code>G = 6.674×10<sup>−11</sup> m<sup>3</sup>/(kg s<sup>2</sup>)</code></p>
-          </FormGroup>
-          <Row className={'mx-1'}>
-            <FormGroup className={'col-5 px-0'}>
-              <label htmlFor={'cl'}>Lift Coefficient (Cl)</label>
-              <Input id={'cl'} type={'number'} required={true} step={'any'}/>
-            </FormGroup>
-            <FormGroup className={'col-5 px-0 ml-auto'}>
-              <label htmlFor={'A'}>Total Blade Area (A) (m<sup>2</sup>)</label>
-              <Input id={'A'} type={'number'} required={true} step={'any'}/>
-            </FormGroup>
-          </Row>
-          <Row className={'mx-1'}>
-            <FormGroup className={'col-5 px-0'}>
-              <label htmlFor={'p'}>Atmospheric Density (ρ) (kg m<sup>-3</sup>)</label>
-              <Input id={'p'} type={'number'} required={true} step={'any'}/>
-            </FormGroup>
-            <FormGroup className={'col-5 px-0 ml-auto'}>
-              <label htmlFor={'m1'}>Mass of the Celestial Body (m<sub>1</sub>) (kg)</label>
-              <Input id={'m1'} type={'number'} required={true} step={'any'}/>
-            </FormGroup>
-          </Row>
-          <Row className={'mx-1'}>
-            <FormGroup className={'col-5 px-0'}>
-              <label htmlFor={'v'}>Blade Velocity (v) (m s<sup>-1</sup>)</label>
-              <Input id={'v'} type={'number'} required={true} step={'any'}/>
-            </FormGroup>
-            <FormGroup className={'col-5 px-0 ml-auto'}>
-              <label htmlFor={'m2'}>Mass of the Hand Helicopter (m<sub>2</sub>) (kg)</label>
-              <Input id={'m2'} type={'number'} required={true} step={'any'}/>
-            </FormGroup>
-          </Row>
           <FormGroup className={'col-5 px-0'}>
-            <label htmlFor={'Cd'}>Drag Coefficient (C<sub>D</sub>)</label>
-            <Input id={'Cd'} type={'number'} required={true} step={'any'}/>
-          </FormGroup>
-          <FormGroup>
-            <label htmlFor={'r'}>Distance between the centre of m<sub>1</sub> and m<sub>2</sub> (r) (m)</label>
-            <Input id={'r'} type={'number'} required={true} step={'any'}/>
+            <label htmlFor={'Cl'}>Lift Coefficient (C<sub>L</sub>)</label>
+            <Input id={'Cl'} type={'number'} required={true} step={'any'}/>
           </FormGroup>
           <Button color={'primary'} outline={true} type={'submit'}>Calculate!</Button>
         </Form>
